9e8d4d65b436b86fd33498707aa1a9ebe693f11f
Guilherme Monteiro <86750421+Guimonteirol@users.noreply.github.com>
2021-08-29 15:38:50 -0300

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double valor1 = 10;
+        double valor2 = 20;
+        double cal = 0;
+        
+        cal = valor1 + valor2;
+        
+        System.out.println("Resultado: "+ cal);
+        
+    }
+    
+}
